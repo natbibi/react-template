929b08fc22b2db8ff4498ea5483b6d191cd5d1ef
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-03 19:43:59 +0100

    create template navigation with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom';
+import * as Pages from './pages';
+import { Header, Footer } from './layout'
 
-const App = () => <h1>Hello World!</h1>
+// import './style.css';
 
-export default App
+function App(){
+
+    return(
+      <>
+        <Header />
+           <Switch>
+          <Route exact path="/">
+            <Pages.Home />
+          </Route>
+          <Route path="/about">
+            <Pages.About />
+          </Route>
+          {/* <Route path="/contact">
+            <Pages.Contact />
+              </Route> */}
+          <Route>
+            <Pages.NotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </>
+    );
+  
+  };
+  
+export default App;
